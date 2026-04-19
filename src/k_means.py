c2354eb15b71aca15f2a168426d96c85709269bd
noumanhafeez <noumanhafeez095@gmail.com>
2026-04-19 13:12:52 +0500

    refactor: add logging integeration for easier debugging

--- src/k_means.py
+++ src/k_means.py
@@ -1,15 +1,24 @@
 import random
 from src.calculate_distance import distance
+from utils.logger import get_logger
+
+logger = get_logger("kmeans")
+
 
 def kmeans(pixels, K, max_iterations=10):
 
-    centroids = random.sample(pixels, K)
+    logger.info(f"Starting K-Means with K={K}, max_iterations={max_iterations}")
 
-    for _ in range(max_iterations):
+    centroids = random.sample(pixels, K)
+    logger.info(f"Initial centroids selected")
+
+    for iteration in range(max_iterations):
+
+        logger.info(f"Iteration {iteration + 1} started")
 
         clusters = [[] for _ in range(K)]
 
-        # Assign pixels to nearest centroid
+        # Assign pixels
         for pixel in pixels:
             distances = [distance(pixel, c) for c in centroids]
             cluster_index = distances.index(min(distances))
@@ -18,9 +27,10 @@ def kmeans(pixels, K, max_iterations=10):
         # Update centroids
         new_centroids = []
 
-        for cluster in clusters:
+        for index, cluster in enumerate(clusters):
 
             if len(cluster) == 0:
+                logger.warning(f"Cluster {index} is empty. Choosing random centroid.")
                 new_centroids.append(random.choice(pixels))
             else:
                 r = sum(p[0] for p in cluster) / len(cluster)
@@ -30,8 +40,10 @@ def kmeans(pixels, K, max_iterations=10):
                 new_centroids.append((int(r), int(g), int(b)))
 
         if new_centroids == centroids:
+            logger.info(f"Converged at iteration {iteration + 1}")
             break
 
         centroids = new_centroids
 
+    logger.info("K-Means completed")
     return centroids
