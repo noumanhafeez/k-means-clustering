87df60347d955eeff19b47274fd9598e80a6c27e
noumanhafeez <noumanhafeez095@gmail.com>
2026-04-19 13:54:10 +0500

    chore: add sample image for clustering

--- src/visualize_image.py
+++ src/visualize_image.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from utils.logger import get_logger
+from config import SHOW_CENTROIDS
 
 logger = get_logger("visualizer")
 
@@ -11,29 +12,45 @@ def show_images(original, compressed, centroids):
     fig, axes = plt.subplots(1, 2, figsize=(12, 6))
 
     axes[0].imshow(original)
-    axes[0].set_title("Original Image")
+    axes[0].set_title("Original")
     axes[0].axis("off")
 
     axes[1].imshow(compressed)
-    axes[1].set_title("Compressed Image")
+    axes[1].set_title("Compressed")
     axes[1].axis("off")
 
     plt.tight_layout()
     plt.show()
 
-    logger.info("Displaying dominant colors")
+    if SHOW_CENTROIDS:
 
-    plt.figure(figsize=(8, 2))
+        logger.info("Displaying centroid RGB values")
 
-    for i, color in enumerate(centroids):
-        plt.fill_between(
-            [i, i + 1],
-            0,
-            1,
-            color=[c / 255 for c in color]
+        for i, color in enumerate(centroids, start=1):
+            logger.info(f"Centroid {i}: {color}")
+
+        fig, ax = plt.subplots(figsize=(10, 2))
+
+        for i, color in enumerate(centroids):
+
+            rgb = [c / 255 for c in color]
+
+            ax.barh(
+                y=0,
+                width=1,
+                left=i,
+                color=rgb
             )
 
-        plt.xlim(0, len(centroids))
-        plt.title("Dominant Colors")
-        plt.axis("off")
+            ax.text(
+                i + 0.1,
+                0,
+                str(color),
+                va="center",
+                fontsize=10
+            )
+
+        ax.set_xlim(0, len(centroids))
+        ax.axis("off")
+        plt.title("Dominant Colors / Centroids")
         plt.show()
